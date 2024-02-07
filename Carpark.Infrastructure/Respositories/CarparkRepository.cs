56b321385d6126f61514ac30902362a96608f655
Lê Minh Quân <quanlm@vmogroup.com>
2024-02-07 14:48:28 +0700

    Commit

--- Carpark.Infrastructure/Respositories/CarparkRepository.cs
+++ Carpark.Infrastructure/Respositories/CarparkRepository.cs
@@ -16,13 +16,20 @@ namespace Carpark.Infrastructure.Respositories
 
         public async Task<PaginatedResponseModel<CarPark>> GetListCarpark(SearchingCarparkInfoRequest request)
         {
-
-            return await GetPaginatedDataByRequest(request, includes: new Expression<Func<CarPark, object>>[]
-            {
-                i => i.CarParkTypeNavigation,
-                i => i.FreeParkingNavigation,
-                i => i.NightParkingNavigation
-            }, orderBy: o => o.OrderByDescending(x => x.CreatedAt));
+            return await GetPaginatedDataByRequest(request,
+                filter: f =>
+                    (!request.CarParkType.HasValue || f.CarParkType == (short)request.CarParkType.Value) &&
+                    (!request.ParkingTypeSystem.HasValue || f.ParkingTypeSystem == (short)request.ParkingTypeSystem.Value) &&
+                    (!request.ShortTermParkingType.HasValue || f.ShortTermParkingType == (short)request.ShortTermParkingType.Value) &&
+                    (!request.FreeParkingType.HasValue || f.FreeParkingType == (short)request.FreeParkingType.Value) &&
+                    (!request.NightParkingType.HasValue || f.NightParkingType == (short)request.NightParkingType.Value) &&
+                    (!request.GantryHeight.HasValue || f.GantryHeight >= request.GantryHeight.Value),
+                includes: new Expression<Func<CarPark, object>>[]
+                {
+                    i => i.CarParkTypeNavigation,
+                    i => i.FreeParkingNavigation,
+                    i => i.NightParkingNavigation
+                }, orderBy: o => o.OrderByDescending(x => x.CreatedAt));
         }
     }
 }
